ca8f4497f2cffe3145777d127423afaae8b53e61
Marco Mendão <mac.mendao@gmail.com>
2019-11-13 11:25:40 +0000

    add payshop service

--- README.md
+++ README.md
@@ -8,11 +8,20 @@ HiPay SDK for Python. HiPay is a payment gateway platform. For more info about t
 pip install hipay-python-sdk
 ```
 
-### Use
+### MB Use
 
 ```
+from hipay_python_sdk.HiPayMBService import HiPayMBService
 hipay_mb_service = HiPayMBService(username="****", password="****")
 ```
+
+### Payshop Use
+
+```
+from hipay_python_sdk.HiPayPayshopService import HiPayPayshopService
+hipay_payshop_service = HiPayPayshopService(username="****", password="****")
+```
+
 
 ### Get MB Reference
 Generate a MB reference with the amount provided.
@@ -22,12 +31,28 @@ mb_reference = self.hipay_mb_service.get_reference_mb(email="email@mal.com", amo
 **Note:** For more info check the documentation [here.](https://trello-attachments.s3.amazonaws.com/5d0795d0a145ea1c06ca85d9/5dc2e959c28e25755811551a/0254737f7fc7a9226130194c22a209ac/multibanco_payshop_ManualTecnico_2.5.pdf)
 
 
-### Get Reference Info
+### Get Reference Info MB
 Check status about the MB Payment reference. Useful if you want to check if the payment have been preformed.
 ```
 reference_info = self.hipay_mb_service.get_info_reference(mb_reference["reference"])
 ```
 **Note:** For more info check the documentation [here.](https://trello-attachments.s3.amazonaws.com/5d0795d0a145ea1c06ca85d9/5dc2e959c28e25755811551a/0254737f7fc7a9226130194c22a209ac/multibanco_payshop_ManualTecnico_2.5.pdf)
+
+### Get Payshop Reference
+Generate a MB reference with the amount provided.
+```
+payshop_reference = self.hipay_payshop_service.get_reference_payshop(email="email@mal.com", amount=10)
+```
+**Note:** For more info check the documentation [here.](https://trello-attachments.s3.amazonaws.com/5d0795d0a145ea1c06ca85d9/5dc2e959c28e25755811551a/0254737f7fc7a9226130194c22a209ac/multibanco_payshop_ManualTecnico_2.5.pdf)
+
+
+### Get Reference Info Payshop
+Check status about the MB Payment reference. Useful if you want to check if the payment have been preformed.
+```
+reference_info = self.hipay_payshop_service.get_info_reference(mb_reference["reference"])
+```
+**Note:** For more info check the documentation [here.](https://trello-attachments.s3.amazonaws.com/5d0795d0a145ea1c06ca85d9/5dc2e959c28e25755811551a/0254737f7fc7a9226130194c22a209ac/multibanco_payshop_ManualTecnico_2.5.pdf)
+
 
 ### Build and Publish
 
--- test.py
+++ test.py
@@ -1,15 +1,19 @@
 import unittest
-from HiPayMBService import HiPayMBService
+from hipay_python_sdk.HiPayMBService import HiPayMBService
+from hipay_python_sdk.HiPayPayshopService import HiPayPayshopService
 
-class HiPayTesy(unittest.TestCase):
+
+class HiPayTest(unittest.TestCase):
     def setUp(self):
         self.hipay_mb_service = HiPayMBService(username="*", password="*")
+        self.hipay_payshop_service = HiPayPayshopService(username="*", password="*")
 
     def tearDown(self):
         pass
 
     def testGetReferenceMB(self):
-        mb_reference = self.hipay_mb_service.get_reference_mb(email="mac.mendao@gmail.com", amount=10, sendEmailBuyer=False)
+        mb_reference = self.hipay_mb_service.get_reference_mb(email="mac.mendao@gmail.com", amount=10,
+                                                              sendEmailBuyer=False)
 
         self.assertIsNotNone(mb_reference)
         self.assertIsNotNone(mb_reference["reference"])
@@ -17,10 +21,30 @@ class HiPayTesy(unittest.TestCase):
         self.assertEqual(mb_reference["amountOut"], 10)
         self.assertIsNotNone(mb_reference["entity"])
 
-    def testGetReferenceInfo(self):
-        mb_reference = self.hipay_mb_service.get_reference_mb(email="mac.mendao@gmail.com", amount=10, sendEmailBuyer=False)
+    def testGetReferenceInfoMB(self):
+        mb_reference = self.hipay_mb_service.get_reference_mb(email="mac.mendao@gmail.com", amount=10,
+                                                              sendEmailBuyer=False)
 
         reference_info = self.hipay_mb_service.get_info_reference(mb_reference["reference"])
+
+        self.assertIsNotNone(reference_info)
+        self.assertIsNotNone(reference_info["paid"])
+        self.assertIsNotNone(reference_info["status"])
+
+    def testGetReferencePayshop(self):
+        mb_reference = self.hipay_payshop_service.get_reference_payshop(email="mac.mendao@gmail.com", amount=10,
+                                                              sendEmailBuyer=False)
+
+        self.assertIsNotNone(mb_reference)
+        self.assertIsNotNone(mb_reference["reference"])
+        self.assertIsNotNone(mb_reference["amountOut"])
+        self.assertEqual(mb_reference["amountOut"], 10)
+
+    def testGetReferenceInfoPayshop(self):
+        mb_reference = self.hipay_payshop_service.get_reference_payshop(email="mac.mendao@gmail.com", amount=10,
+                                                              sendEmailBuyer=False)
+
+        reference_info = self.hipay_payshop_service.get_info_reference(mb_reference["reference"])
 
         self.assertIsNotNone(reference_info)
         self.assertIsNotNone(reference_info["paid"])
